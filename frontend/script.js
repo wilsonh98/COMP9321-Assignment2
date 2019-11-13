472a7811eeccc3397383b3d08a9315533a2b3131
Bibhor Sharma <bibhorsharma7@gmail.com>
2019-11-13 13:36:44 +1100

    started on frontend

---
+++ frontend/script.js
@@ -0,0 +1,7 @@
+document.getElementById("buy").addEventListener("click", function() {
+    alert("buying");
+})
+
+document.getElementById("sell").addEventListener("click", function() {
+    alert("selling");
+})
